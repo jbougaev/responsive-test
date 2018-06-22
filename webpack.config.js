2a1b96044043cf47804d555dc72c4f69df4db949
julia-bougaev <julie.bougaev@gmail.com>
2018-06-22 14:05:39 -0400

    css fixes

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+﻿const path = require('path'),
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+ HtmlWebpackPlugin = require('html-webpack-plugin'),
+ WebpackMd5Hash = require('webpack-md5-hash'),
+ MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+ CleanWebpackPlugin = require('clean-webpack-plugin'),
+ CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist', {}),
+         //new ExtractTextPlugin(
+         //  {filename: 'style.[hash].css', disable: false, allChunks: true }
+         //),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash(),
+        new CopyWebpackPlugin([{
+            from: 'assets/images',
+            to: 'assets/images',
+            toType: 'dir'
+        }], { debug: 'debug' }
+        )
+    ]
+};
