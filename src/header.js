86a3ef13db052da39b64a3f1703916c59f7acd80
julia.bougaev <julia.bougaev@blueprintsys.com>
2018-06-22 21:01:30 -0400

    content fixes

--- src/header.js
+++ src/header.js
@@ -1,20 +1,37 @@
-﻿const Header = () => {
-    const init = () => {
-        let link = document.querySelector(".menu-btn");
-        link.addEventListener("click", () => {
-            let menu = document.querySelector(".menu-container > div");
-            menu.classList.toggle("is-visible");
-           
-        });
-    }
-    
+﻿//const Header = () => {
+//    const init = () => {
+//        let link = document.querySelector(".menu-btn");
+//        link.addEventListener("click", () => {
+//            let menu = document.querySelector(".menu-container > div");
+//            menu.classList.toggle("is-visible");
+//           
+//        });
+//    }
+//    
+//
+//    return {
+//        init: init
+//      
+//    }
+//
+//
+//}
+
+const Header = function(){
+        const init = () => {
+       let link = document.querySelector(".menu-btn");
+       link.addEventListener("click", () => {
+           let menu = document.querySelector(".menu-container > div");
+           menu.classList.toggle("is-visible");
+          
+       });
+   }
+   
 
     return {
         init: init
-      
+     
     }
-
-
 }
 
 export default Header;
